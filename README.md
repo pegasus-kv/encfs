58cf2b6d5cc0c09ce5115417bb03fb91a613e7dd
Yingchun Lai <laiyingchun@apache.org>
2023-11-08 19:59:52 +0800

    Implement the data encryption plugin for RocksDB (#1)
    Most of the code are migrated from https://github.com/pegasus-kv/rocksdb/commits/v8.5.3-pegasus-encrypt, there is no functional changes, but just refactor to use the plugin architecture [1] of RocksDB.
    
    1. https://github.com/facebook/rocksdb/tree/main/plugin#readme

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
 # encfs
-An encryption plugin for RocksDB.
+
+The `AESEncryptionProvider` of `EncryptionFileSystem` is an encryption plugin for RocksDB. It depends on OpenSSL to show how an external plugin can bring its dependencies into the RocksDB build. It provides a factory function in a header file to show integration with RocksDB header includes. It can also be enabled in text-based options to demonstrate use of the static registration framework.
+
+## Build
+Download the RocksDB code from the official repository. Currently, version 8.5.3 is used as the benchmark for testing.
+
+```
+git clone --depth 1 --branch v8.5.3 https://github.com/facebook/rocksdb.git
+```
+
+The code first needs to be linked under RocksDB's `plugin/` directory. In your RocksDB directory, run:
+
+```
+$ pushd ./plugin/
+$ git clone https://github.com/pegasus-kv/encfs.git
+```
+
+Next, we can build and install RocksDB with this plugin as follows:
+
+```
+$ popd
+$ make clean && DEBUG_LEVEL=0 ROCKSDB_PLUGINS="encfs" make -j32 db_bench install
+```
+
+Build by cmake and check the functionality:
+```
+#!/usr/bin/env bash
+
+set -ex
+
+# 1. build
+mkdir build && cd build
+cmake -DWITH_LZ4=1 -DCMAKE_BUILD_TYPE=Debug -DWITH_TESTS=1 -DROCKSDB_BUILD_SHARED=0 -DROCKSDB_PLUGINS=encfs ..
+make -j $(nproc)
+
+# 2. run encfs_test
+./encfs_test
+
+# 3. Run tests and tools
+opts=("id=AES;hex_instance_key=0123456789ABCDEF0123456789ABCDEF;method=AES128CTR"
+      "id=AES;hex_instance_key=0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF;method=AES192CTR"
+      "id=AES;hex_instance_key=0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF;method=AES256CTR"
+      "id=AES;hex_instance_key=0123456789ABCDEF0123456789ABCDEF;method=SM4CTR")
+for opt in ${opts[*]}; do
+  echo "${opt}"
+
+  # 3.1. Run all tests
+  # Set env ENCRYPTED_ENV and run all tests
+  export ENCRYPTED_ENV=${opt}
+  ctest -j $(nproc) -V
+  unset ENCRYPTED_ENV
+
+  uri='provider={${uri}};id=EncryptedFileSystem'
+  echo "${uri}"
+
+  # 3.2. Run env_basic_test and env_test tests
+  # Set env TEST_FS_URI and run env_basic_test and env_test
+  export TEST_FS_URI=${uri}
+  ./env_basic_test --gtest_filter=*CustomEnv*
+  ./env_test --gtest_filter=CreateEnvTest.CreateEncryptedFileSystem
+  unset TEST_FS_URI
+
+  # 3.3. Run benchmarks
+  ./db_bench --fs_uri="${uri}" --benchmarks="fillseq,readrandom,readseq" --compression_type=lz4 --num=1000000
+
+  # 3.4. Run ldb tools
+  ./tools/ldb --fs_uri="${uri}" --db=/tmp/rocksdbtest-1000/dbbench/ put k v
+  ./tools/ldb --fs_uri="${uri}" --db=/tmp/rocksdbtest-1000/dbbench/ get k
+  ls -l /tmp/rocksdbtest-1000/dbbench | grep "log" | awk '{print $NF}' | xargs -i ./tools/ldb --fs_uri="${uri}" dump_wal --walfile=/tmp/rocksdbtest-1000/dbbench/{} | head
+  ./tools/ldb --fs_uri="${uri}" --db=/tmp/rocksdbtest-1000/dbbench/ scan --hex | head
+  ./tools/ldb --fs_uri="${uri}" --db=/tmp/rocksdbtest-1000/dbbench/ dump --hex | head
+  ./tools/ldb --fs_uri="${uri}" --db=/tmp/rocksdbtest-1000/dbbench/ manifest_dump | head
+  ./tools/ldb --fs_uri="${uri}" --db=/tmp/rocksdbtest-1000/dbbench/ list_live_files_metadata | head
+done
+```
+
+## Tool usage
+
+For RocksDB binaries (such as the `ldb`, `db_bench` we built above), the plugin can be enabled through configuration. `ldb` and `db_bench` in particular takes a `--fs_uri` where we can specify "encfs" , which is the name registered by this plugin. Example usage:
+
+```
+$ ./tools/ldb --fs_uri="provider={id=AES;hex_instance_key=0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF;method=AES256CTR};id=EncryptedFileSystem" --db=/tmp/rocksdbtest-1000/dbbench/ scan --hex | head
+$ ./db_bench --benchmarks=fillrandom --fs_uri="provider={id=AES;hex_instance_key=0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF;method=AES256CTR};id=EncryptedFileSystem" --compression_type=lz4
+```
+
+## Application usage
+
+The plugin's interface is also exposed to applications, which can enable it either through configuration or through code. Here is an example instantiating the plugin in code.
+
+```
+$ cat <<EOF >./test.cpp
+#include <cstdio>
+#include <memory>
+
+#include <rocksdb/convenience.h>
+#include <rocksdb/env.h>
+#include <rocksdb/status.h>
+
+int main() {
+  std::string fs_uri = "provider={id=AES;hex_instance_key=0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF;method=AES256CTR};id=EncryptedFileSystem";
+  rocksdb::Env* raw_env = nullptr;
+  std::shared_ptr<rocksdb::Env> env_guard;
+  rocksdb::Status s = rocksdb::Env::CreateFromUri(rocksdb::ConfigOptions(), /*env_uri=*/ "", fs_uri, &raw_env, &env_guard);
+  if (!s.ok()) {
+    fprintf(stderr, "fs_uri='%s'\n", fs_uri.c_str());
+    fprintf(stderr, "CreateFromUri() failed with error %s\n", s.ToString().c_str());
+    return 1;
+  }
+
+  s = rocksdb::WriteStringToFile(raw_env, "test_data", "test.txt", /*should_sync=*/ true);
+  if (!s.ok()) {
+    fprintf(stderr, "WriteStringToFile() failed with error %s\n", s.ToString().c_str());
+    return 1;
+  }
+
+  return 0;
+}
+EOF
+$ g++ -std=c++17 -o test test.cpp -lrocksdb -lpthread -llz4 -lcrypto -u encfs_reg
+$ ./test
+```
+
+# Thanks
+
+Some of the implementations are inspired by [Apache Kudu](https://github.com/apache/kudu) and [tikv/rocksdb](https://github.com/tikv/rocksdb)
---
+++ encfs.h
@@ -0,0 +1,184 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#pragma once
+#include <gtest/gtest.h>
+#include <openssl/evp.h>
+
+#include <string>
+
+#include "rocksdb/env_encryption.h"
+
+namespace ROCKSDB_NAMESPACE {
+class AESCTRCipherStream;
+
+using evp_ctx_unique_ptr =
+    std::unique_ptr<EVP_CIPHER_CTX, decltype(&EVP_CIPHER_CTX_free)>;
+
+// The encryption method supported.
+enum class EncryptionMethod : int {
+  kUnknown = 0,
+  kAES128_CTR = 1,
+  kAES192_CTR = 2,
+  kAES256_CTR = 3,
+  // OpenSSL support SM4 after 1.1.1 release version.
+  kSM4_CTR = 4,
+};
+
+static const std::unordered_map<std::string, EncryptionMethod>
+    encryption_method_enum_map = {{"", EncryptionMethod::kUnknown},
+                                  {"AES128CTR", EncryptionMethod::kAES128_CTR},
+                                  {"AES192CTR", EncryptionMethod::kAES192_CTR},
+                                  {"AES256CTR", EncryptionMethod::kAES256_CTR},
+#if OPENSSL_VERSION_NUMBER > 0x1010100fL && !defined(OPENSSL_NO_SM4)
+                                  {"SM4CTR", EncryptionMethod::kSM4_CTR}
+#endif
+};
+
+// Get the key size of the encryption method.
+size_t KeySize(EncryptionMethod method);
+
+// Get the block size of the encryption method.
+size_t BlockSize(EncryptionMethod method);
+
+// Get the OpenSSL EVP_CIPHER according to the encryption method.
+const EVP_CIPHER* GetEVPCipher(EncryptionMethod method);
+
+// Get the last OpenSSL error message.
+std::string GetOpenSSLErrors();
+
+// Convert an OpenSSL error to an IOError Status.
+#define OPENSSL_RET_NOT_OK(call, msg)                  \
+  if (UNLIKELY((call) <= 0)) {                         \
+    return Status::IOError((msg), GetOpenSSLErrors()); \
+  }
+
+Status NewAESCTRCipherStream(EncryptionMethod method,
+                             const std::string& file_key,
+                             const std::string& file_key_iv,
+                             std::unique_ptr<AESCTRCipherStream>* result);
+
+// The cipher stream for AES-CTR encryption.
+class AESCTRCipherStream : public BlockAccessCipherStream {
+ public:
+  AESCTRCipherStream(const EncryptionMethod method, std::string file_key,
+                     uint64_t iv_high, uint64_t iv_low)
+      : method_(method),
+        file_key_(std::move(file_key)),
+        initial_iv_high_(iv_high),
+        initial_iv_low_(iv_low) {}
+
+  ~AESCTRCipherStream() override = default;
+
+  size_t BlockSize() override;
+
+  enum class EncryptType : int { kDecrypt = 0, kEncrypt = 1 };
+
+  Status Encrypt(uint64_t file_offset, char* data, size_t data_size) override {
+    return Cipher(file_offset, data, data_size, EncryptType::kEncrypt);
+  }
+
+  Status Decrypt(uint64_t file_offset, char* data, size_t data_size) override {
+    return Cipher(file_offset, data, data_size, EncryptType::kDecrypt);
+  }
+
+ protected:
+  // Following methods required by BlockAccessCipherStream is unused.
+
+  void AllocateScratch(std::string& /*scratch*/) override {
+    // should not be called.
+    assert(false);
+  }
+
+  Status EncryptBlock(uint64_t /*block_index*/, char* /*data*/,
+                      char* /*scratch*/) override {
+    return Status::NotSupported("EncryptBlock should not be called.");
+  }
+
+  Status DecryptBlock(uint64_t /*block_index*/, char* /*data*/,
+                      char* /*scratch*/) override {
+    return Status::NotSupported("DecryptBlock should not be called.");
+  }
+
+ private:
+  Status Cipher(uint64_t file_offset, char* data, size_t data_size,
+                EncryptType encrypt_type);
+
+  const EncryptionMethod method_;
+  const std::string file_key_;
+  const uint64_t initial_iv_high_;
+  const uint64_t initial_iv_low_;
+};
+
+struct AESEncryptionOptions {
+  std::string instance_key;
+  EncryptionMethod method;
+  AESEncryptionOptions(std::string k = "",
+                       EncryptionMethod m = EncryptionMethod::kUnknown)
+      : instance_key(std::move(k)), method(m) {
+    assert(instance_key.size() == KeySize(method));
+  }
+};
+
+// TODO(yingchun): Is it possible to derive from CTREncryptionProvider?
+// The encryption provider for AES-CTR encryption.
+class AESEncryptionProvider : public EncryptionProvider {
+ public:
+  AESEncryptionProvider();
+  ~AESEncryptionProvider() override = default;
+
+  static const char* kClassName() { return "AES"; }
+  const char* Name() const override { return kClassName(); }
+  bool IsInstanceOf(const std::string& name) const override;
+
+  size_t GetPrefixLength() const override { return kDefaultPageSize; }
+
+  Status CreateNewPrefix(const std::string& /*fname*/, char* prefix,
+                         size_t prefix_length) const override;
+
+  Status AddCipher(const std::string& /*descriptor*/, const char* /*cipher*/,
+                   size_t /*len*/, bool /*for_write*/) override {
+    return Status::NotSupported();
+  }
+
+  Status CreateCipherStream(
+      const std::string& fname, const EnvOptions& options, Slice& prefix,
+      std::unique_ptr<BlockAccessCipherStream>* result) override;
+
+ private:
+  FRIEND_TEST(AESEncryptionProviderDeterministicTest, DecryptFileKey);
+  FRIEND_TEST(AESEncryptionProviderDeterministicTest, EncryptFileKey);
+  FRIEND_TEST(AESEncryptionProviderTest, EncryptAndDecryptFileKey);
+  FRIEND_TEST(AESEncryptionProviderTest, WriteAndReadEncryptionHeader);
+
+  struct FileEncryptionInfo {
+    EncryptionMethod method = EncryptionMethod::kUnknown;
+    std::string key;
+    std::string iv;  // TODO(yingchun): not used yet
+  };
+
+  Status WriteEncryptionHeader(char* header_buf) const;
+  Status ReadEncryptionHeader(Slice prefix,
+                              FileEncryptionInfo* file_info) const;
+
+  Status EncryptFileKey(char* file_key, size_t file_key_size) const;
+  Status DecryptFileKey(char* file_key, size_t file_key_size) const;
+
+  AESEncryptionOptions aes_options_;
+};
+
+}  // namespace ROCKSDB_NAMESPACE
